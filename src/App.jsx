2855909311db4572dd029faa75303cc023ebdc89
Pallavi <patilpallavi1895@gmail.com>
2025-04-01 16:47:00 +0530

    logout features

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import logoImg from './assets/logo.png'
-// import logoImg2 from './assets/TalentSift.png'
-// import Dashboard from './Dashboard'
 import LoginForm from './components/LoginForm'
 import Layout from './components/Layout'
 
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import au_logo from '../assets/au_logo.png';
 import au_logo2 from '../assets/au_logo2.png';
+import { useNavigate } from 'react-router-dom';
+
 
 const Dashboard = () => {
+const navigate = useNavigate();
+
+    const handlogout=()=>{
+        navigate('/'); 
+    }
+
   return (
     <div>
       <div className='flex place-content-between bg-amber-50 p-3'>
@@ -19,19 +27,6 @@ const Dashboard = () => {
             <p>Debit Card Services</p>
             <p>Debit Card Services</p>
         </div>
-        {/* <div className='flex gap-3'>
-          <div><img src={logoImg2}/></div>
-          <div className='flex gap-[20px] items-center pl-[20px]'>
-            <img src={hamburger} className='items-center'/>
-            <input className='border border-gray-500'/>
-          </div>
-        
-        </div>
-        <div className='flex'>
-            <h3>Welcome User</h3>
-            <img src={User} className='items-center' />
-        </div> */}
-
 
       </div>
       <div className='flex justify-between'>
@@ -63,9 +58,9 @@ const Dashboard = () => {
       Make Payment
     </button>
   </div>
-  <div className="w-max ml-2">
-    <button className="w-max mt-2 bg-[#f56403] text-white py-2 px-4 rounded-md">
-      Login
+  <div className="w-max">
+    <button className="w-max mt-2 bg-[#f56403] text-white py-2 px-4 rounded-md" onClick={handlogout}>
+      Logout
     </button>
   </div>
 </div>
